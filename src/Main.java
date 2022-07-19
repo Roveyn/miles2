88730119d630fd0c7288744f634d1ccd5ae2e111
Rita Oveyan <rita_oveyan@mail.ru>
2022-07-19 21:32:46 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 2000;
+        int x = amount / 20;
+
+        String name = "Bonus";
+
+        System.out.println(name + " равен " + x);
+    }
+
+}
